608ef2419282ffd02455e0e6ec13920d0eae32ea
Julia Foresti <juliajforesti@gmail.com>
2020-10-20 17:14:37 -0300

    Updates

---
+++ precache-manifest.384292f78fa5c89bcacd6fad7d2bf6c6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "83878800f9f4e71e5862c4d4930e6c18",
+    "url": "/randomizer/index.html"
+  },
+  {
+    "revision": "14a3886599f60b305048",
+    "url": "/randomizer/static/css/main.c2fa2ffd.chunk.css"
+  },
+  {
+    "revision": "6eaf13454b8bb4e5669a",
+    "url": "/randomizer/static/js/2.deaac309.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/randomizer/static/js/2.deaac309.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "14a3886599f60b305048",
+    "url": "/randomizer/static/js/main.2a5c96dc.chunk.js"
+  },
+  {
+    "revision": "8be28df881d95fdf88ff",
+    "url": "/randomizer/static/js/runtime-main.12dc38ce.js"
+  },
+  {
+    "revision": "0cd0ca797ca5aa4e99eed6e8f40f4830",
+    "url": "/randomizer/static/media/hxagon.0cd0ca79.png"
+  }
+]);
